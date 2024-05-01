43ff81df783b1cbe3c1a70acd0af269d21ed764e
viditviionn <vidit.viionn@gmail.com>
2024-05-01 15:11:05 +0530

    callup description

--- src/utils/projects.js
+++ src/utils/projects.js
@@ -8,69 +8,63 @@ import Inergy from "../assets/Inergy.png";
 import callUpImg from "../assets/callupcalendar.png";
 
 export const PROJECTS = [
-    {
-        img: callUpImg,
-        title: "CallupCalendar",
-        weblink: "https://callupcalendar.com/",
-        description:
-          "CallupCalendar is a fun side project inspired by true joy from the national pastime -- experiencing games in person. Here user can catch the Top MLB Prospects Locally.",
-      },
-      {
-        img: abbypagesProImg,
-        title: "AbbyPages",
-        weblink: "https://www.abbypages.com/",
-        description:
-          "On-demand services web application, just like Yelp, allows customers to book the services in one click. It also provides User Reviews and Recommendations of Best Restaurants, Shopping, Nightlife, Food, Entertainment, Things to Do, Services and More. Facilitates scheduling the Appointment, Hire for Job on an hourly or fixed amount, Payments, Live tracking of orders, KYC system.",
-      },
-    {
-      img: Inergy,
-      title: "Innergy",
-      pslink: "https://play.google.com/store/apps/details?id=com.imih.innergy",
-      description:
-        "Discover holistic well-being with Innergy, This app is for reducing stress, staying active, and embracing a plant-based lifestyle.",
-    },
-   
-    {
-      img: krustty,
-      title: "Krustty",
-      weblink: "https://krustty.com/",
-      pslink: "https://play.google.com/store/apps/details?id=com.krustty",
-      description:
-        "Book your table and order your meals in advance Find offers directly on the Krustty app.   Invite your loved ones and share the bill.",
-    },
-    {
-      img: belayLink,
-      title: "Belaylink",
-      weblink: "https://dev.belaylink.com/",
-      description:
-        "Create your sports profile and find your stoke. Update what you are up to, and let people reach out to you. Connect and do sports together. Follow friends and stay posted for their activities.",
-    },
-    {
-      img: hdfc,
-      title: "Banks Marketing Audit",
-      pslink: "https://play.google.com/store/apps/details?id=com.audit.Kreate",
-      description:
-        "This app is focused on Auditing Marketing related aspects. With periodic marketing audit institutions can monitor, manage and improve seamless customer experience across their branches.",
-    },
-   
-   
-    {
-      img: nokelsTv,
-      title: "Nokels TV",
-      pslink:
-        "https://play.google.com/store/apps/details?id=com.nokelstv&pli=1/",
-      description:
-        "NokelsTV is a Video streaming platform that distributes African movies and tv shows. NokelsTV is a subscription based app and release movies and tv shows on a monthly basis",
-    },
-    {
-      img: slushDating,
-      title: "Slush Dating",
-      pslink:
-        "https://www.slushdating.com/",
-      description:
-        "Create your profile. Book onto an event. Go through a series of 3-minute video dates. During your dates, you can decide whether you like or dislike that person",
-    },
-   
-   
-    
-  ];
+  {
+    img: callUpImg,
+    title: "CallupCalendar",
+    weblink: "https://callupcalendar.com/",
+    description:
+      "CallupCalendar is an Baseball enthusiasts platform inspired by true joy from the national pastime -- experiencing games in person. Here user can catch the Top MLB Prospects Locally. The platform is built from scratch using MERN stack and hosted serverless in AWS. ",
+  },
+  {
+    img: abbypagesProImg,
+    title: "AbbyPages",
+    weblink: "https://www.abbypages.com/",
+    description:
+      "On-demand services web application, just like Yelp, allows customers to book the services in one click. It also provides User Reviews and Recommendations of Best Restaurants, Shopping, Nightlife, Food, Entertainment, Things to Do, Services and More. Facilitates scheduling the Appointment, Hire for Job on an hourly or fixed amount, Payments, Live tracking of orders, KYC system.",
+  },
+  {
+    img: Inergy,
+    title: "Innergy",
+    pslink: "https://play.google.com/store/apps/details?id=com.imih.innergy",
+    description:
+      "Discover holistic well-being with Innergy, This app is for reducing stress, staying active, and embracing a plant-based lifestyle.",
+  },
+
+  {
+    img: krustty,
+    title: "Krustty",
+    weblink: "https://krustty.com/",
+    pslink: "https://play.google.com/store/apps/details?id=com.krustty",
+    description:
+      "Book your table and order your meals in advance Find offers directly on the Krustty app.   Invite your loved ones and share the bill.",
+  },
+  {
+    img: belayLink,
+    title: "Belaylink",
+    weblink: "https://dev.belaylink.com/",
+    description:
+      "Create your sports profile and find your stoke. Update what you are up to, and let people reach out to you. Connect and do sports together. Follow friends and stay posted for their activities.",
+  },
+  {
+    img: hdfc,
+    title: "Banks Marketing Audit",
+    pslink: "https://play.google.com/store/apps/details?id=com.audit.Kreate",
+    description:
+      "This app is focused on Auditing Marketing related aspects. With periodic marketing audit institutions can monitor, manage and improve seamless customer experience across their branches.",
+  },
+
+  {
+    img: nokelsTv,
+    title: "Nokels TV",
+    pslink: "https://play.google.com/store/apps/details?id=com.nokelstv&pli=1/",
+    description:
+      "NokelsTV is a Video streaming platform that distributes African movies and tv shows. NokelsTV is a subscription based app and release movies and tv shows on a monthly basis",
+  },
+  {
+    img: slushDating,
+    title: "Slush Dating",
+    pslink: "https://www.slushdating.com/",
+    description:
+      "Create your profile. Book onto an event. Go through a series of 3-minute video dates. During your dates, you can decide whether you like or dislike that person",
+  },
+];
